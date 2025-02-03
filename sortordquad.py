c645cfe6a1fe6dd8dff9a7fc043d1ff0b88f1709
coreimnu <complexrealimaginary@gmail.com>
2025-02-02 22:38:32 -0800

    Add files via upload

--- sortordquad.py
+++ sortordquad.py
@@ -1,67 +1,77 @@
 import array
 import random
 
+def triangular(n):
+    return (n+1) * n // 2
+
 def sortbubble(arr):
-    print(f'{arr} initial')
-    for sublength in range(len(arr), 1, -1):
-        swapcount = 0
-        print(f'sublength {sublength} start')
-        for i in range(sublength-1):
-            if arr[i] > arr[i+1]:
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                swapcount += 1
-                print(f'{arr} swap {i} {i+1}')
-        if swapcount == 0:
-            print(f'swapcount {swapcount} break end')
+    print(f'{arr} initial array')
+    tswap = 0
+    for i in range(len(arr)-1):
+        print(f'start loop {i}')
+        nswap = 0
+        for j in range(len(arr)-1, i, -1):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+                nswap += 1
+                print(f'{arr} swapped position {j-1} and {j}')
+        print(f'ncomp == {len(arr)-1-i}, nswap == {nswap}, end loop {i}')
+        if nswap == 0:
             break
-        else:
-            print(f'swapcount {swapcount} end')
-    print()
+        tswap += nswap
+    print(f'{arr} tcomp == {triangular(len(arr)-1) - triangular(len(arr)-i-2)}, tswap == {tswap}, final array')
 
 def sortinsertion(arr):
-    print(f'{arr} initial')
-    for sublength in range(len(arr), 1, -1):
-        swapcount = 0
-        print(f'sublength {sublength} start')
-        for i in range(sublength-2, len(arr)-1):
-            if arr[i] > arr[i+1]:
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                swapcount += 1
-                print(f'{arr} swap {i} {i+1}')
+    print(f'{arr} initial array')
+    tcomp, tswap = 0, 0
+    for i in range(len(arr)-1):
+        print(f'start loop {i}')
+        ncomp, nswap = 0, 0
+        for j in range(i+1, 0, -1):
+            ncomp += 1
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+                nswap += 1
+                print(f'{arr} swapped position {j-1} and {j}')
             else:
                 break
-        print(f'swapcount {swapcount} end')
-    print()
+        print(f'ncomp == {ncomp}, nswap == {nswap}, end loop {i}')
+        tcomp += ncomp
+        tswap += nswap
+    print(f'{arr} tcomp == {tcomp}, tswap == {tswap}, final array')
 
 def sortexchange(arr):
-    print(f'{arr} initial')
-    for sublength in range(len(arr), 1, -1):
-        swapcount = 0
-        print(f'sublength {sublength} start')
-        for i in range(sublength-1):
-            if arr[i] > arr[sublength-1]:
-                arr[i], arr[sublength-1] = arr[sublength-1], arr[i]
-                swapcount += 1
-                print(f'{arr} swap {i} {sublength-1}')
-        print(f'swapcount {swapcount} end')
-    print()
+    print(f'{arr} initial array')
+    tswap = 0
+    for i in range(len(arr)-1):
+        print(f'start loop {i}')
+        nswap = 0
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
+                nswap += 1
+                print(f'{arr} swapped position {i} and {j}')
+        print(f'ncomp == {len(arr)-i-1}, nswap == {nswap}, end loop {i}')
+        tswap += nswap
+    print(f'{arr} tcomp == {triangular(len(arr)-1)}, tswap == {tswap}, final array')
 
 def sortselection(arr):
-    print(f'{arr} initial')
-    for sublength in range(len(arr), 1, -1):
-        maxindex = sublength - 1
-        indexswapcount = 0
-        print(f'sublength {sublength} start')
-        for i in range(sublength-1):
-            if arr[i] > arr[maxindex]:
-                maxindex = i
-                indexswapcount += 1
-                print(f'maxindex {maxindex}')
-        if indexswapcount != 0:
-            arr[maxindex], arr[sublength-1] = arr[sublength-1], arr[maxindex]
-            print(f'{arr} swap {sublength-1} {maxindex}')
-        print(f'indexswapcount {indexswapcount} end')
-    print()
+    print(f'{arr} initial array')
+    tswap = 0
+    for i in range(len(arr)-1):
+        print(f'start loop {i}')
+        minindex = i
+        nswap = 0
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[minindex]:
+                minindex = j
+        if minindex != i:
+            arr[i], arr[minindex] = arr[minindex], arr[i]
+            nswap += 1
+            print(f'{arr} swapped position {i} and {minindex}')
+        print(f'ncomp == {len(arr)-i-1}, nswap == {nswap}, end loop {i}')
+        tswap += nswap
+    print(f'{arr} tcomp == {triangular(len(arr)-1)}, tswap == {tswap}, final array')
 
 arr = array.array('b', range(7))
 
